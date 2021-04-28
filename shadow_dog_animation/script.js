e10c7dfb785628137536561b6370d16c559ec820
MartinFjellstrom <martin.fjallstrom@gmail.com>
2021-04-28 12:30:22 +0000

    Shadow dog animation finished

---
+++ shadow_dog_animation/script.js
@@ -0,0 +1,88 @@
+playerState = 'run';
+const dropDown = document.getElementById('animations')
+dropDown.addEventListener('change', function(e){
+    playerState = e.target.value;
+})
+
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+const canW = canvas.width = 600;
+const canH = canvas.height = 600;
+
+const playerImg = new Image();
+playerImg.src = 'shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+
+let gameFrame = 0;
+const staggerFrames = 7;
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },
+    {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'ko',
+        frames: 12,
+    },
+    {
+        name: 'getHit',
+        frames: 4,
+    }
+];
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+    for (let j = 0; j < state.frames; j++) {
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({ x: positionX, y: positionY });
+    }
+    spriteAnimations[state.name] = frames;
+});
+
+function animate() {
+    ctx.clearRect(0, 0, canW, canH);
+    let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
+    ctx.drawImage(playerImg, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+animate()
+
+
+
+let minframe = 0;
+let mixframe = 0;
